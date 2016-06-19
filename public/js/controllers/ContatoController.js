6a013a7997d03b116c680cdd20c6f097d6e0b686
EdersonLRF <edersonluis.rf@gmail.com>
2016-06-19 20:29:12 -0300

    4.17 o objeto

---
+++ public/js/controllers/ContatoController.js
@@ -0,0 +1,5 @@
+// public/js/controllers/ContatoController.js
+
+angular.module('contatooh').controller('ContatoController', function($scope, $routeParams) {
+		console.log($routeParams.contatoId);
+});
