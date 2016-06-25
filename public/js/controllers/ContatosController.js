c660b25ecfab4e8e0ca83d4dcc2bd8605e6739c3
EdersonLRF <edersonluis.rf@gmail.com>
2016-06-25 11:53:37 -0300

    4.19 a diretiva ng-repeat

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -9,4 +9,22 @@ angular.module('contatooh').controller('ContatosController', function($scope) {
 	$scope.incrementa = function() {
 		$scope.total++;
 	};
+
+	$scope.contatos = [
+		{
+			"_id": 1,
+			"nome": "Contato Angular 1",
+			"email": "cont1@empresa.com.br"
+		},
+		{
+			"_id": 2,
+			"nome": "Contato Angular 2",
+			"email": "cont2@empresa.com.br"
+		},
+		{
+			"_id": 1,
+			"nome": "Contato Angular 3",
+			"email": "cont3@empresa.com.br"
+		}
+	];
 });
