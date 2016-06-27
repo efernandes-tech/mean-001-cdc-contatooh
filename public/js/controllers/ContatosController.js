5162ed877c3892e22514170076c563002e93910f
edersonlrf <edersonluis.rf@gmail.com>
2016-06-27 20:55:27 -0300

    5.4 obtendo contatos com

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,32 +1,28 @@
 // public/js/controllers/ContatosController.js
 
 // Criando o controller passando as dependencias.
-angular.module('contatooh').controller('ContatosController', function($scope) {
-	// Adicionando a propriedade que sera utilizada pela view.
+angular.module('contatooh').controller('ContatosController', function($scope, $http) {
+	// Iniciando as propriedades que serao utilizadas pela view.
+	$scope.contatos = [];
 	$scope.total = 0;
+	$scope.filtro = '';
 
 	// Funcao chamada por diretivas.
 	$scope.incrementa = function() {
 		$scope.total++;
 	};
 
-	$scope.contatos = [
-		{
-			"_id": 1,
-			"nome": "Contato Angular 1",
-			"email": "cont1@empresa.com.br"
-		},
-		{
-			"_id": 2,
-			"nome": "Contato Angular 2",
-			"email": "cont2@empresa.com.br"
-		},
-		{
-			"_id": 1,
-			"nome": "Contato Angular 3",
-			"email": "cont3@empresa.com.br"
-		}
-	];
-
-	$scope.filtro = "";
+	// O '$http' e diferente do '$.ajax()' do jQuery, ele retorna uma 'promise'.
+	// A 'promise' (promessa) e um objeto que fornecera um resultado futuro.
+	// Dessa forma vc evita o 'Callback HELL' e a 'Pyramid Of Doom'.
+	$http.get('/contatos')
+		// Executado quando 'fulfilled'.
+		.success(function(data) {
+			$scope.contatos = data;
+		})
+		// Executado quando 'rejected' ou 'failed'.
+		.error(function(statusText) {
+			console.log('Não foi possível obter a lista de contatos.');
+			console.log(statusText);
+		});
 });
