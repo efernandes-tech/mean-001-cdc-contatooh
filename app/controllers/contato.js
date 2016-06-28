1ff77280c47b5a9fe0e0649e8500e1ee819f9bcc
EdersonLRF <edersonluis.rf@gmail.com>
2016-06-27 22:40:05 -0300

    5.7 adicionando rota de exclusao ao express

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -27,5 +27,17 @@ module.exports = function() {
 		contato ? res.json(contato) : res.status(404).send('Contato não encontrado');
 	};
 
+	controller.removeContato = function(req, res) {
+
+		var idContato = req.params.id;
+
+		console.log('API: removeContato: ' + idContato);
+
+		contatos = contatos.filter(function(contato) {
+			return contato._id != idContato;
+		});
+		res.send(204).end();
+	};
+
 	return controller;
 };
