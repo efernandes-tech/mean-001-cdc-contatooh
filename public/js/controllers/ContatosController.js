1c34e2b5acc67ac8a81e2a74a997e4a7797ea8ff
edersonlrf <edersonluis.rf@gmail.com>
2016-06-27 21:29:40 -0300

    5.6 consumindo rest endpoints com

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,22 +1,30 @@
 // public/js/controllers/ContatosController.js
 
 // Criando o controller passando as dependencias.
-angular.module('contatooh').controller('ContatosController', function($scope, $resource) {
-// angular.module('contatooh').controller('ContatosController', function($scope, $http) {
+angular.module('contatooh').controller('ContatosController', function($scope, $resource) { // ... $scope, $http) {
 	// Iniciando as propriedades que serao utilizadas pela view.
 	$scope.contatos = [];
-	// $scope.total = 0;
 	$scope.filtro = '';
 
-	// Funcao chamada por diretivas.
-	// $scope.incrementa = function() {
-	// 	$scope.total++;
-	// };
+	// O '$resource' um objeto com funcoes proprias para REST.
+	var Contato = $resource('/contatos');
+
+	// Realiza uma requisicao GET por baixo dos panos.
+	var promise = Contato.query().$promise;
+
+	promise
+		.then(function(contatos) {
+			$scope.contatos = contatos;
+		})
+		.catch(function(erro) {
+			console.log('Não foi possível obter a lista de contatos');
+			console.log(erro);
+		});
 
 	// O '$http' e diferente do '$.ajax()' do jQuery, ele retorna uma 'promise'.
 	// A 'promise' (promessa) e um objeto que fornecera um resultado futuro.
 	// Dessa forma vc evita o 'Callback HELL' e a 'Pyramid Of Doom'.
-	$http.get('/contatos')
+	/*$http.get('/contatos')
 		// Executado quando 'fulfilled'.
 		.success(function(data) {
 			$scope.contatos = data;
@@ -25,5 +33,12 @@ angular.module('contatooh').controller('ContatosController', function($scope, $r
 		.error(function(statusText) {
 			console.log('Não foi possível obter a lista de contatos.');
 			console.log(statusText);
-		});
+		});*/
+
+	// $scope.total = 0;
+
+	// Funcao chamada por diretivas.
+	// $scope.incrementa = function() {
+	// 	$scope.total++;
+	// };
 });
