6ecb3ccb6830bd1ef02eadb37ede8989c535fe76
EdersonLRF <ederson.luis@tegis.com.br>
2017-07-22 15:51:47 -0300

    7.13 modificando nossa view para exibir emergencias

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -35,4 +35,8 @@ angular.module('contatooh').controller('ContatoController', function($scope, $ro
 					console.log(erro);
 				});
 		};
+
+		Contato.query(function(contatos) {
+			$scope.contatos = contatos;
+		});
 });
