e38e7d110ae03f2c58130fb04ca54fec9b21b7b1
edersonlrf <edersonluis.rf@gmail.com>
2017-07-23 15:26:00 -0300

    7.8 buscando pelo id

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -18,7 +18,20 @@ module.exports = function(app) {
 			);
 	};
 
-	controller.obtemContato = function(req, res) {};
+	controller.obtemContato = function(req, res) {
+		var _id = req.params.id;
+		Contato.findById(_id).exec()
+			.then(
+				function(contato) {
+					if (!contato) throw new Error("Contato não encontrado!");
+					res.json(contato)
+				},
+				function(erro) {
+					console.log(erro);
+					res.status(404).json(erro)
+				}
+			);
+	};
 
 	controller.removeContato = function(req, res) {};
 
