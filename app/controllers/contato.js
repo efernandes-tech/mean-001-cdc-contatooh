df034b3a7fea38af22e79b42881c75348bf143ae
EdersonLRF <ederson.luis@tegis.com.br>
2017-07-23 15:21:42 -0300

    7.7 buscando documentos

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -5,7 +5,18 @@ module.exports = function(app) {
 
 	var controller = {};
 
-	controller.listaTodos = function(req, res) {};
+	controller.listaContatos = function(req, res) {
+		var promise = Contato.find().exec()
+			.then(
+				function(contatos) {
+					res.json(contatos);
+				},
+				function(erro) {
+					console.error(erro)
+					res.status(500).json(erro);
+				}
+			);
+	};
 
 	controller.obtemContato = function(req, res) {};
 
