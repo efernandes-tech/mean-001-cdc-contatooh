ed2ba3a3fec999a485fe101730260c8324c7030c
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-21 14:30:36 -0200

    10.6 atalho para tarefas

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,29 @@
-// contatooh/Gruntfile.js
+// Gruntfile.js
 
 module.exports = function(grunt) {
+    grunt.initConfig({
+        copy: {
+            project: {
+                expand: true,
+                cwd: '.',
+                src: [
+                    '**',
+                    '!Gruntfile.js',
+                    '!package.json',
+                    '!public/bower.json'
+                ],
+                dest: 'dist'
+            }
+        },
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['dist']);
+    grunt.registerTask('dist', ['clean', 'copy']);
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 };
