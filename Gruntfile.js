5c39ffaf73b1f50b4045bd5890c67a26b140ad1e
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-21 14:45:27 -0200

    10.8 grunt-usemin: minificação e concatenação com grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,4 +26,8 @@ module.exports = function(grunt) {
     grunt.registerTask('dist', ['clean', 'copy']);
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
 };
