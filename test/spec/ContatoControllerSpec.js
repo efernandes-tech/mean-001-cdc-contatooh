4fc6d3e12d7983cb847c1d8795dcfb1577a00b50
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-21 17:28:14 -0200

    save

--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -2,12 +2,17 @@
 
 describe("ContatoController", function() {
 
-    var $scope;
+    var $scope, $httpBackend;
 
     beforeEach(function() {
         module('contatooh');
-        inject(function($injector) {
+        inject(function($injector, _$httpBackend_){
             $scope = $injector.get('$rootScope').$new();
+            $httpBackend = _$httpBackend_;
+            $httpBackend.when('GET', '/contatos/1')
+                .respond({_id: '1'});
+            $httpBackend.when('GET', '/contatos')
+                .respond([{}]);
         });
     });
 
@@ -18,6 +23,17 @@ describe("ContatoController", function() {
             "$scope" : $scope
         });
         expect($scope.contato._id).toBeUndefined();
+    }));
+
+    it("Deve preencher o Contato quando"
+        + "parâmetro de rota for passado",
+    inject(function($controller) {
+        $controller('ContatoController', {
+            '$routeParams': {contatoId: 1},
+            '$scope': $scope
+        });
+        $httpBackend.flush();
+        expect($scope.contato._id).toBeDefined();
     }));
 
 });
