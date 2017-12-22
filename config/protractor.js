a8c541507a6387822dc9c8c7772428aba757f204
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-22 17:18:20 -0200

    save

---
+++ config/protractor.js
@@ -0,0 +1,14 @@
+// config/protractor.js
+
+exports.config = {
+    specs: ['../test/e2e/*.js'],
+    onPrepare: function() {
+        browser.driver.get('http://localhost:3000');
+        browser.driver.findElement(by.id('entrar')).click();
+        browser.driver.findElement(by.id('login_field'))
+            .sendKeys('email-de-teste');
+        browser.driver.findElement(by.id('password'))
+            .sendKeys('senha-do-email-de-teste');
+        browser.driver.findElement(by.name('commit')).click();
+    }
+};
