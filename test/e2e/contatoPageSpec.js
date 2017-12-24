02271c91c6ed93e773cdeddb7ed2613716b558a1
edersonlrf <edersonluis.rf@gmail.com>
2017-12-24 15:10:29 -0200

    11.12 pageobject: melhorando a legibilidade e manutencao de testes

--- test/e2e/contatoPageSpec.js
+++ test/e2e/contatoPageSpec.js
@@ -1,19 +1,20 @@
 // test/e2e/contatoPageSpec.js
 
+var ContatoPage = new require('./pages/contatoPage');
+
 describe('Cadastro de contatos', function() {
+    var pagina = new ContatoPage();
+
     beforeEach(function() {
-        browser.get('http://localhost:3000/#/contato');
+        pagina.visitar();
     });
+
     it('Deve cadastrar um contato', function() {
         var aleatorio = Math.floor((Math.random() * 10000000) + 1);
-        var nome = 'teste' + aleatorio;
-        var email = 'teste@email' + aleatorio;
-        element(by.model('contato.nome')).sendKeys(nome);
-        element(by.model('contato.email')).sendKeys(email);
-        element(by.css('option[value="0"]')).click();
-        element(by.css('.btn-primary')).click();
-        expect(element(by.binding('mensagem.texto'))
-            .getText())
-            .toContain('sucesso');
+        pagina.digitarNome('teste' + aleatorio);
+        pagina.digitarEmail('teste@email' + aleatorio);
+        pagina.selecionarPrimeiraEmergenciaDaLista();
+        pagina.salvar();
+        expect(pagina.obterMensagem()).toContain('sucesso');
     });
 });
--- test/e2e/contatosPageSpec.js
+++ test/e2e/contatosPageSpec.js
@@ -1,26 +1,24 @@
 // test/e2e/contatosPageSpec.js
 
+var ContatosPage = new require('./pages/contatosPage');
+
 describe('Página principal', function() {
+    var pagina = new ContatosPage();
 
     beforeEach(function() {
-        browser.get('http://localhost:3000/#/contatos');
+        pagina.visitar();
     });
 
     it('Deve estar logado', function() {
-        element(by.id('usuario-logado')).getText()
-            .then(function(texto) {
-                expect(texto.trim().length).toBeGreaterThan(0);
-            });
+        pagina.obterUsuarioLogado().then(function(texto) {
+            expect(texto.trim().length).toBeGreaterThan(0);
+        });
     });
 
     it('Deve remover um contato da lista', function() {
-        var totalAntes = element.all(by.repeater('contato in contatos')).count();
-
-        element(by.repeater('contato in contatos').row(0))
-            .element(by.css('.btn'))
-            .click();
-
-        var totalDepois = element.all(by.repeater('contato in contatos')).count();
+        var totalAntes = pagina.obterTotalDeItensDaLista();
+        pagina.removerPrimeiroItemDaLista();
+        var totalDepois = pagina.obterTotalDeItensDaLista();
         expect(totalDepois).toBeLessThan(totalAntes);
     });
 });
