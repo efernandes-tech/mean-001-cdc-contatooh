637bf8bfafe098da9d86dcf30b6ec5b7822f55f9
edersonlrf <edersonluis.rf@gmail.com>
2017-12-24 11:50:26 -0200

    save

--- config/protractor.js
+++ config/protractor.js
@@ -1,14 +1,15 @@
 // config/protractor.js
 
 exports.config = {
-    specs: ['../test/e2e/*.js'],
+    specs: ['../test/e2e/*Spec.js'],
     onPrepare: function() {
         browser.driver.get('http://localhost:3000');
         browser.driver.findElement(by.id('entrar')).click();
         browser.driver.findElement(by.id('login_field'))
             .sendKeys('email-de-teste');
         browser.driver.findElement(by.id('password'))
-            .sendKeys('senha-do-email-de-teste');
+            .sendKeys('eder2332');
         browser.driver.findElement(by.name('commit')).click();
     }
 };
+
