efc098b1ab91eae29060f10edda331a2900617f7
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-26 16:43:31 -0200

    save

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,48 +2,26 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
-        copy: {
-            project: {
-                expand: true,
-                cwd: '.',
-                src: [
-                    '**',
-                    '!Gruntfile.js',
-                    '!package.json',
-                    '!public/bower.json'
-                ],
-                dest: 'dist'
-            }
-        },
-        clean: {
-            dist: {
-                src: 'dist'
-            }
-        },
         usemin : {
-            html: 'dist/app/views/**/*.ejs'
+            html: 'app/views/**/*.ejs'
         },
         useminPrepare: {
             options: {
-                root: 'dist/public',
-                dest: 'dist/public'
+                root: 'public',
+                dest: 'public'
             },
-            html: 'dist/app/views/**/*.ejs'
+            html: 'app/views/**/*.ejs'
         },
         ngAnnotate: {
             scripts: {
                 expand: true,
-                src: ['dist/public/js/**/*.js']
+                src: ['public/js/**/*.js']
             },
         }
     });
 
-    grunt.registerTask('default', ['dist', 'minifica']);
-    grunt.registerTask('dist', ['clean', 'copy']);
     grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
