8fa9d1b23fd6c79d0823561153302240744c1e1e
edersonlrf <edersonluis.rf@gmail.com>
2017-12-26 22:07:46 -0200

    save

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-// config/auth.js
-
-module.exports = function(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        res.status('401').json('Não autorizado');
-    }
-};
