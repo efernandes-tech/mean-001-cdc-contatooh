2a0a0374b9f291d6bd1c9905dee0e2712fd5242d
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-27 17:46:26 -0200

    save

--- config/protractor.js
+++ config/protractor.js
@@ -14,14 +14,14 @@ exports.config = {
 
     specs: ['../test/e2e/**/*Spec.js'],
     onPrepare: function() {
-        browser.driver.get('http://localhost:3000/#/auth');/*.then(function(){*/
+        browser.driver.get('http://localhost:3000/#/auth').then(function(){
             browser.driver.findElement(by.id('entrar')).click();
             browser.driver.findElement(by.id('login_field'))
                 .sendKeys(config.seleniumUser);
             browser.driver.findElement(by.id('password'))
                 .sendKeys(config.seleniumUserPassword);
             browser.driver.findElement(by.name('commit')).click();
-        /*});*/
+        });
     }
 };
 
