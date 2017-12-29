95b8530d39e97a88a7d1ca64df62b705ac565549
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-29 16:45:02 -0200

    save

--- config/env/production.js
+++ config/env/production.js
@@ -5,5 +5,7 @@ module.exports = {
     db: process.env.MONGODB_URI + 'contatooh',
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    port: process.env.PORT
+    port: process.env.PORT,
+    address: process.env.OPENSHIFT_NODEJS_IP,
+    domain: process.env.OPENSHIFT_APP_DNS
 };
