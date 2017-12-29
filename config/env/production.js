d49140b6092b34ff11d90456d1d173ea3c6ad785
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-29 15:17:23 -0200

    save

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,8 @@ module.exports = {
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     port: process.env.PORT,
-    address: process.env.OPENSHIFT_NODEJS_IP,
-    domain: process.env.OPENSHIFT_APP_DNS
+    // address: process.env.OPENSHIFT_NODEJS_IP,
+    // domain: process.env.OPENSHIFT_APP_DNS
+    address: 'localhost',
+    domain: 'localhost:3000'
 };
