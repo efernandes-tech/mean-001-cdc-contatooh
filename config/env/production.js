7da2ea46896dfdb65035a90e56f870f10d26caf7
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-29 12:10:58 -0200

    save

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,6 @@ module.exports = {
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     port: process.env.PORT,
-    address: 'https://mean-001-cdc-contatooh.herokuapp.com',
-    domain: 'https://mean-001-cdc-contatooh.herokuapp.com:' + process.env.PORT
+    address: 'mean-001-cdc-contatooh.herokuapp.com',
+    domain: 'mean-001-cdc-contatooh.herokuapp.com:' + process.env.PORT
 };
